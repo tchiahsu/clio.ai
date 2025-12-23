b7929b7d0de6225ead0e4fee87b10f618d5450a2
Tony Hsu Tai <hsutai.c@gmail.com>
2025-12-22 20:58:01 -0500

    dashboard endpoint v1

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { dashboardRouter } from "./api/dashboard/routes.js";
+
+export const app = express();
+
+// Reads raw request body, parses JSON and attaches it to request
+app.use(express.json());
+
+// To Assign a user id since we doing auth last, fake user basically
+app.use((req, _res, next) => {
+    (req as any).user = {userId: 1};
+    next();
+});
+
+// Mount Routers
+app.use("/dashboard", dashboardRouter);
+
+export default app

bd61f5da0f6e6c0a0d0dc28b7b3f6764c682f072
Tony Hsu Tai <hsutai.c@gmail.com>
2025-12-23 19:44:13 -0500

    added docstring to routes

--- backend/src/api/dashboard/routes.ts
+++ backend/src/api/dashboard/routes.ts
@@ -8,16 +8,19 @@ import {
 export const dashboardRouter = Router();
 
 /**
- * GET /dashboard/transaction/summary
+ * GET /dashboard/transaction/totals
+ * must provide statementId as param
  */
 dashboardRouter.get("/transaction/totals", getDashboardSummary);
 
 /**
- * GET /dashboard/transaction/summary
+ * GET /dashboard/transaction/category
+ * must provide statementId as param
  */
 dashboardRouter.get("/transaction/category", getDashboardCategory);
 
 /**
- * GET /dashboard/transaction/summary
+ * GET /dashboard/transaction
+ * must provide statementId as param
  */
 dashboardRouter.get("/transaction", getDashboardFilter);

faa8cb18fe2f83475d5240c0007964c87a3dd209
Tony Hsu Tai <hsutai.c@gmail.com>
2025-12-23 19:43:03 -0500

    removed testing console log

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,11 +12,6 @@ app.use((req, _res, next) => {
     next();
 });
 
-app.use((req, _res, next) => {
-    console.log("INCOMING:", req.method, req.originalUrl);
-    next();
-})
-
 // Mount Routers
 app.use("/dashboard", dashboardRouter);
 

6527e3e309544793e0a8d75da9a3d5be2a5c7716
mishell778 <mishell.r.123@gmail.com>
2025-12-25 21:35:43 -0500

    fixed comments in router.ts file

--- backend/src/api/merchants/routes.ts
+++ backend/src/api/merchants/routes.ts
@@ -10,18 +10,18 @@ export const merchantsRouter = Router();
 
 /**
  * GET /merchants/merchant
- * must provide _______ as param
+ * no param required
  */
 merchantsRouter.get("/merchant", getMerchantsList);
 
 /**
  * GET /merchants/merchant
- * must provide _______ as param
+ * must provide newMerchantName and merchantId as params
  */
 merchantsRouter.patch("/merchant/:id", updateMerchantName);
 
 /**
  * GET /merchants/merchant
- * must provide _______ as param
+ * must provide merchantId as param
  */
 merchantsRouter.get("/merchant/:id", getMerchantsHistory);

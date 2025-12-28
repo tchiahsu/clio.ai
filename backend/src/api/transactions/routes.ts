ac301f62fccf796bf4923a4de868c85944828ec2
Tony Hsu Tai <hsutai.c@gmail.com>
2025-12-27 19:15:26 -0500

    completed endpoints for transaction data

--- backend/src/api/transactions/routes.ts
+++ backend/src/api/transactions/routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import {
-    getTransactionList
+    getTransactionList,
+    getTransactionDetail,
+    patchTransactionCategory,
+    patchTransactionMerchant
 } from "./controller.js";
 
 export const transactionRouter = Router();
@@ -12,3 +15,21 @@ export const transactionRouter = Router();
  * - scope="statement" also provide statementId as param
  */
 transactionRouter.get("/all", getTransactionList);
+
+/**
+ * GET /transaction/detail
+ * must provide userId and transactionId as param
+ */
+transactionRouter.get("/detail", getTransactionDetail);
+
+/**
+ * GET /transaction/category
+ * must provide userId, transactionId and categoryId as param
+ */
+transactionRouter.patch("/category", patchTransactionCategory);
+
+/**
+ * GET /transaction/merchant
+ * must provide userId, transactionId and merchantId as param
+ */
+transactionRouter.patch("/merchant", patchTransactionMerchant);
--- backend/src/api/transactions/sql.ts
+++ backend/src/api/transactions/sql.ts
@@ -3,12 +3,12 @@ import { Pool } from "pg";
 /**
  * Verify that a statement exists and belongs to the user.
  */
-export async function sqlLatestStatementId(pool: Pool, userId: number): Promise<number> {
+export async function sqlLatestStatementId(pool: Pool, userId: number) {
     const res = await pool.query(
         `
         SELECT statement_id FROM statements
         WHERE user_id = $1
-        ORDER BY period_end DESK, statement_id DESC
+        ORDER BY period_end DESC, statement_id DESC
         LIMIT 1
         `,
         [userId]
@@ -35,4 +35,65 @@ export async function sqlAllTransactions(pool: Pool, userId: number) {
     );
 
     return res.rows;
+}
+
+/**
+ * Get transaction details for a single transaction
+ */
+export async function sqlGetTransactionDetail(pool: Pool, transactionId: number, userId: number) {
+    const res = await pool.query(
+        `
+        SELECT t.*, a.account_number, a.bank_name, a.account_type, c.category_name, m.merchant_name FROM transactions t
+        JOIN accounts a ON t.account_id = a.account_id
+        LEFT JOIN categories c ON t.category_id = c.category_id
+        LEFT JOIN merchants m ON t.merchant_id = m.merchant_id
+        WHERE t.transaction_id = $1
+        AND t.user_id = $2
+        `,
+        [transactionId, userId]
+    );
+
+    return res.rows[0] ?? null;
+}
+
+/**
+ * Change category of a transaction
+ */
+export async function sqlPatchTransactionCategory(pool: Pool, userId: number, categoryId: number, transactionId: number) {
+    const res = await pool.query(
+        `
+        UPDATE transactions t
+        SET category_id = c.category_id,
+            category_confidence = 1.0
+        FROM categories c
+        WHERE t.transaction_id = $1
+        AND t.user_id = $2
+        AND c.category_id = $3
+        AND c.user_id = t.user_id
+        RETURNING t.transaction_id, t.category_id, t.category_confidence;
+        `,
+        [transactionId, userId, categoryId]
+    );
+
+    return res.rows[0] ?? null;
+}
+
+/**
+ * Change merchant name
+ */
+export async function sqlPatchTransactionMerchant(pool: Pool, userId: number, transactionId: number, merchantId: number) {
+    const res = await pool.query(
+        `
+        UPDATE transactions t
+        SET merchant_id = m.merchant_id
+        FROM merchants m
+        WHERE t.transaction_id = $1
+        AND t.user_id = $2
+        AND m.merchant_id = $3
+        RETURNING t.transaction_id, t.merchant_id;
+        `,
+        [transactionId, userId, merchantId]
+    );
+
+    return res.rows[0] ?? null;
 }

02e68ca24e88f29d0e3e0a5e4c64023a64a582aa
Tony Hsu Tai <hsutai.c@gmail.com>
2026-01-06 10:20:25 -0500

    data parsing logic

--- backend/src/api/data-parsing.ts
+++ backend/src/api/data-parsing.ts
@@ -1,3 +1,11 @@
-export default function dataParsing(statementId: number, filePath: string) {
+import { PDFParse } from "pdf-parse";
 
+const parser = new PDFParse({ url: 'file:///Users/tonyhsu/Desktop/projects/clio.ai/cc.pdf'})
+
+export async function dataParsing() {
+  const result = await parser.getText();
+  console.log(result.text);
 }
+
+
+dataParsing();

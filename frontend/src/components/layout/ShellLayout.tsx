68ab879b0e0eef478b0a0d422198c99e6492fe83
Tony Hsu Tai <hsutai.c@gmail.com>
2026-06-08 21:09:19 -0400

    shell layout for all pages

---
+++ frontend/src/components/layout/ShellLayout.tsx
@@ -0,0 +1,16 @@
+import Sidebar from "./Sidebar"
+
+interface ShellLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function ShellLayout({ children }: ShellLayoutProps) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1">
+        {children}
+      </main>
+    </div>
+  );
+}
